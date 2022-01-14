bf3c8421b63b78ff22c80a7a388446ff39ed0914
avents <avents@laposte.net>
2022-01-15 00:05:58 +0100

    Flipper in composition api script setup style

--- src/Flipper.vue
+++ src/Flipper.vue
@@ -1,115 +1,99 @@
-<script>
+<script setup>
 import { Flipper } from "flip-toolkit";
-import { h, onBeforeUpdate, onMounted, watch, nextTick, ref, provide } from 'vue'
+import { onBeforeUpdate, onMounted, watch, nextTick, ref, provide } from 'vue'
 
-export default {
-  name: "flipper",
-  props: {
-    className: String,
-    flipKey: [String, Number, Boolean],
-    staggerConfig: Object,
-    spring: {
-      type: [String, Object],
-      default: "noWobble"
-    }
-  },
-  setup(props, { slots }) {
+const props = defineProps({
+  className: String,
+  flipKey: [String, Number, Boolean],
+  staggerConfig: Object,
+  spring: {
+    type: [String, Object],
+    default: "noWobble"
+  }
+})
 
-    const flipInstance = ref(null)
-    const ready = ref(false)
+const flipInstance = ref(null)
+const ready = ref(false)
 
-    const addFlippedElement = ({
-      element,
-      flipId,
-      delayUntil,
-      stagger,
-      shouldFlip,
-      shouldInvert,
-      onStart,
-      onComplete,
-      opacity,
-      scale,
-      translate
-    }) => {
-      flipInstance.value.addFlipped({
-        element,
-        flipId,
-        ...(delayUntil ? { delayUntil } : undefined),
-        ...(stagger ? { stagger } : undefined),
-        ...(shouldFlip ? { shouldFlip } : undefined),
-        ...(shouldInvert ? { shouldInvert } : undefined),
-        ...(onStart ? { onStart } : undefined),
-        ...(onComplete ? { onComplete } : undefined),
-        opacity,
-        scale,
-        translate
-      });
-    }
+const addFlippedElement = ({
+  element,
+  flipId,
+  delayUntil,
+  stagger,
+  shouldFlip,
+  shouldInvert,
+  onStart,
+  onComplete,
+  opacity,
+  scale,
+  translate
+}) => {
+  flipInstance.value.addFlipped({
+    element,
+    flipId,
+    delayUntil,
+    stagger,
+    shouldFlip,
+    shouldInvert,
+    onStart,
+    onComplete,
+    opacity,
+    scale,
+    translate
+  });
+}
 
-    const addInvertedElement = async ({ element, parent, opacity, scale, translate }) => {
+const addInvertedElement = async ({ element, parent, opacity, scale, translate }) => {
+  await nextTick()
+  flipInstance.value.addInverted({
+    element,
+    parent,
+    opacity,
+    scale,
+    translate
+  });
+}
+
+provide('addFlippedElement', addFlippedElement)
+provide('addInvertedElement', addInvertedElement)
+
+const rootEl = ref()
+
+onMounted(() => {
+  console.log(rootEl.value)
+  flipInstance.value = new Flipper({
+    element: rootEl.value,
+    spring: props.spring,
+    ...(props.staggerConfig ? { staggerConfig: props.staggerConfig } : null)
+  });
+  ready.value = true;
+})
+onBeforeUpdate(() => {
+  flipInstance.value.recordBeforeUpdate();
+})
+
+watch(
+  () => props.flipKey,
+  async (newKey, oldKey) => {
+    if (newKey !== oldKey) {
       await nextTick()
-      flipInstance.value.addInverted({
-        element,
-        parent,
-        opacity,
-        scale,
-        translate
-      });
+      flipInstance.value.update(oldKey, newKey);
     }
+  }
+)
 
-    provide('addFlippedElement', addFlippedElement)
-    provide('addInvertedElement', addInvertedElement)
-
-    const rootEl = ref()
-
-    onMounted(() => {
-      console.log(rootEl.value)
-      flipInstance.value = new Flipper({
-        element: rootEl.value,
-        spring: props.spring,
-        ...(props.staggerConfig ? { staggerConfig: props.staggerConfig } : null)
-      });
-      ready.value = true;
-    })
-    onBeforeUpdate(() => {
-      flipInstance.value.recordBeforeUpdate();
-    })
-
-    watch(
-      () =>  props.flipKey,
-      async (newKey, oldKey) => {
-        if (newKey !== oldKey) {
-          await nextTick()
-          flipInstance.value.update(oldKey, newKey);
-        }
-      }
-    )
-
-    watch(
-      () =>  props.staggerConfig,
-      (oldConfig, newConfig) => {
-        if (newConfig !== oldConfig) {
-          flipInstance.value.staggerConfig = newConfig;
-        }
-      }
-    )
-
-    return () => {
-      return h('div',
-        {
-          class: props.className,
-          ref: rootEl
-        },
-        {
-          default: () => {
-            if (ready.value) {
-              return slots.default()
-            }
-          }
-        }
-      )
+watch(
+  () => props.staggerConfig,
+  (oldConfig, newConfig) => {
+    if (newConfig !== oldConfig) {
+      flipInstance.value.staggerConfig = newConfig;
     }
-
-  },
-};
+  }
+)
 </script>
+
+<template>
+  <div ref="rootEl">
+    <slot v-if="ready"></slot>
+  </div>
+</template>
